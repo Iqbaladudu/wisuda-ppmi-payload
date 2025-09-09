86c8e0007541c54869077447157613ca7d9eb72a
Muhammad Iqbal <iqbal.adudu@gmail.com>
2025-09-09 20:54:08 +0300

    done fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,14 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 import createNextIntlPlugin from 'next-intl/plugin'
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  output: 'standalone',
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
     }
-
     return webpackConfig
   },
   typescript: {
